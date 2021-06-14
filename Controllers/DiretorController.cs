f8c5a0fbf62c9b5cbfaea3b14d48f35350113d20
vitoriasilvareis <vitoria.reis@soitic.com>
2021-06-14 19:10:42 -0300

    Tarefa 3

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,16 +13,43 @@ public class DiretorController : ControllerBase {
         _context = context;
     }
 
+    [HttpGet("{id}")]
+        public async Task<ActionResult<Diretor>> Get(int id)
+        {
+            var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+            return Ok(diretor);
+            
+        }
+
     [HttpGet]
     public async Task<List<Diretor>> Get() {
-        return await _context.Diretores.ToListAsync();
+       return await _context.Diretores.ToListAsync();
     }
 
     [HttpPost]
     public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
-        _context.Diretores.Add(diretor);
+       _context.Diretores.Add(diretor);
         await _context.SaveChangesAsync();
 
         return Ok(diretor);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Diretor>> Put(int id, [FromBody] Diretor diretor)
+    {
+        diretor.Id = id;
+        _context.Diretores.Update(diretor);
+        await _context.SaveChangesAsync();
+
+        return Ok(diretor); 
+    } 
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var diretor = _context.Diretores.FirstOrDefault(diretor => diretor.Id == id);
+        _context.Remove(diretor);
+        _context.SaveChangesAsync();
+        return Ok();
     }
 }
