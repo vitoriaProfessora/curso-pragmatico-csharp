c115028c1cffeba1c5c9c10f8ec51a0970b289b0
vitoriasilvareis <vitoria.reis@soitic.com>
2021-06-14 21:30:05 -0300

    Adiciona métodos de Get(id) Put e Delete

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,47 +7,49 @@ using Microsoft.EntityFrameworkCore;
 [Route("[controller]")]
 public class DiretorController : ControllerBase {
     private readonly ApplicationDbContext _context;
+
     public DiretorController(ApplicationDbContext context) {
         _context = context;
     }
 
-    [HttpGet("{id}")]
-        public async Task<ActionResult<Diretor>> Get(int id)
-        {
-            var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
-            return Ok(diretor);
-            
-        }
-
+    // GET api/diretores
     [HttpGet]
     public async Task<List<Diretor>> Get() {
-       return await _context.Diretores.ToListAsync();
+        return await _context.Diretores.ToListAsync();
     }
 
+    // GET api/diretores/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Diretor>> Get(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
+        return Ok(diretor);
+    }
+
+    // POST api/diretores
     [HttpPost]
     public async Task<ActionResult<Diretor>> Post([FromBody] Diretor diretor) {
-       _context.Diretores.Add(diretor);
+        _context.Diretores.Add(diretor);
         await _context.SaveChangesAsync();
 
         return Ok(diretor);
     }
 
+    // PUT api/diretores/{id}
     [HttpPut("{id}")]
-    public async Task<ActionResult<Diretor>> Put(int id, [FromBody] Diretor diretor)
-    {
+    public async Task<ActionResult<Diretor>> Put(long id, [FromBody] Diretor diretor) {
         diretor.Id = id;
         _context.Diretores.Update(diretor);
         await _context.SaveChangesAsync();
 
-        return Ok(diretor); 
-    } 
+        return Ok(diretor);
+    }
 
+    // DELETE api/diretores/{id}
     [HttpDelete("{id}")]
-    public ActionResult Delete(int id)
-    {
-        var diretor = _context.Diretores.FirstOrDefault(diretor => diretor.Id == id);
+    public async Task<ActionResult> Delete(long id) {
+        var diretor = await _context.Diretores.FirstOrDefaultAsync(diretor => diretor.Id == id);
         _context.Remove(diretor);
-        _context.SaveChangesAsync();
-        return Ok();
+        await _context.SaveChangesAsync();
+        return Ok(diretor);
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -2,7 +2,7 @@ public class Filme {
     public Filme(string titulo) {
         Titulo = titulo;
     }
-    public int Id { get; set; }
+    public long Id { get; set; }
     public string Titulo { get; set; }
     public string Ano { get; set; }
     public string Genero { get; set; }
