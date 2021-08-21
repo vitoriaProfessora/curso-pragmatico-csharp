4ae30a4f235764c7d5b4ddaab6b0433cfc42d05d
vitoriasilvareis <vitoria.reis@soitic.com>
2021-08-21 09:56:27 -0300

    Adiciona projeto de teste unitario

---
+++ MeuAppTestes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MeuAppTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void QuandoPassarDoisEDoisOResultadoTemQueSerQuatro()
+        {
+            // Assert.Equal(4, Add(2, 2));
+        }
+    }
+}
